c23dbd4ddeae2b7902ec3c3f381d49ea00c9eff0
Maite Rivas <maiterivas09@gmail.com>
2022-06-01 00:16:56 -0500

    .py file comments

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,27 @@
-#PyBank Section for Homework
+#  PyBank Section for Homework
+
+#  Calculate each of the following:
+
+# -The total number of months included in the dataset
+
+# -The net total amount of "Profit/Losses" over the entire period
+
+# -The changes in "Profit/Losses" over the entire period, and then the average of those changes
+
+# -The greatest increase in profits (date and amount) over the entire period
+
+# -The greatest decrease in profits (date and amount) over the entire period
+
+#Your analysis should look similar to the following:
+
+  #```text
+  #Financial Analysis
+  #----------------------------
+  #Total Months: 86
+  #Total: $22564198
+  #Average Change: $-8311.11
+  #Greatest Increase in Profits: Aug-16 ($1862002)
+  #Greatest Decrease in Profits: Feb-14 ($-1825558)
+  #```
+
+# In addition, your final script should both print the analysis to the terminal and export a text file with the results.
